adb4eb554af0318e223f763d9377a745c1937028
rudraksh-GKMIT <rudraksh@gkmit.co>
2025-11-20 22:42:33 +0530

    fix : api path from http to https

--- src/constant/keys.js
+++ src/constant/keys.js
@@ -1,4 +1,4 @@
 export const STORAGE_KEYS = {
     ACCESS_TOKEN: "access_token",
 };
-export const API_BASE = "http://51.20.109.219:8000/api";
+export const API_BASE = "https://affairs-publication-expertise-consolidation.trycloudflare.com/api";
