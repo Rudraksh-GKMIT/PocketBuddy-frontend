7ef9a72cc9720c939c88ea166f043580a5d7b196
rudraksh-GKMIT <rudraksh@gkmit.co>
2025-11-20 23:42:39 +0530

    fix: update the api key

--- src/constant/keys.js
+++ src/constant/keys.js
@@ -1,4 +1,4 @@
 export const STORAGE_KEYS = {
     ACCESS_TOKEN: "access_token",
 };
-export const API_BASE = "https://affairs-publication-expertise-consolidation.trycloudflare.com/api";
+const API_BASE = "/api";
